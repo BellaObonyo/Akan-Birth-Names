aa9319fd8d9b0200e32c9a46e9af1b6489986fd6
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-06-10 22:14:39 +0300

    added javascript file

---
+++ main.js
@@ -0,0 +1,31 @@
+var days_of_week = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+var male_names = new Array('Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame');
+var female_names = new Array('Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama');
+var akanName = '';
+
+function checkDayofWeek(){
+	inputValidation();
+	dob = document.getElementById('dob').value;
+	gender = document.querySelector('input[name="gender"]:checked').value;
+	
+	var dobDate = new Date(dob);
+    var indexOfDay = dobDate.getDay();
+	dayOfWeek = days_of_week[indexOfDay];
+	if(gender == 'male'){
+		akanName = male_names[indexOfDay];
+	} else if (gender == 'female'){
+		akanName = female_names[indexOfDay];
+	}
+	document.getElementById('results').innerHTML = "Your Akan name is: " + akanName;
+}
+
+function inputValidation() {
+	if (document.getElementById('dob').value == '') {
+		alert("Please select a date");
+	} 
+	if (!atLeastOneRadio()) {
+		alert("Please select a your gender");
+	}
+}
+
+function atLeast
